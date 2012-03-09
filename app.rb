c4ccb32bd1a83df3210f9bfab5a8ba12b08a49ff
Rune Madsen <rune@runemadsen.com>
2012-03-09 16:39:33 -0500

    init

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+# DataMapper.auto_upgrade!
+
+set :root, File.dirname(__FILE__)
+
+get '/' do
+  "This is the response from your GET request"
+end
+
+post '/' do
+  "This is the response from your POST request"
+end
